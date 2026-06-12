a8206829e4a4f0d49c251cd58ff43f3da52e4be6
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-06-12 09:51:48 +0530

    added sql schema for table messages and converstaion

--- server/sql/init.sql
+++ server/sql/init.sql
@@ -1,46 +1,46 @@
 CREATE TABLE users (
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    created_at DATETIME(3) NULL,
-    updated_at DATETIME(3) NULL,
-    deleted_at DATETIME(3) NULL,
-    user_id VARCHAR(30) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) DEFAULT 'user',
-    is_deleted BOOLEAN DEFAULT FALSE,
-    is_blocked BOOLEAN DEFAULT FALSE,
+    id              BIGINT              UNSIGNED            AUTO_INCREMENT      PRIMARY KEY,
+    created_at      DATETIME(3)         NULL,
+    updated_at      DATETIME(3)         NULL,
+    deleted_at      DATETIME(3)         NULL,
+    user_id         VARCHAR(30)         NOT NULL            UNIQUE,
+    password        VARCHAR(255)        NOT NULL,
+    role            VARCHAR(255)        DEFAULT 'user',
+    is_deleted      BOOLEAN             DEFAULT FALSE,
+    is_blocked      BOOLEAN             DEFAULT FALSE,
 
     INDEX idx_users_deleted_at (deleted_at)
 );
 
 CREATE TABLE rooms (
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    created_at DATETIME(3) NULL,
-    updated_at DATETIME(3) NULL,
-    deleted_at DATETIME(3) NULL,
+    id              BIGINT          UNSIGNED        AUTO_INCREMENT      PRIMARY KEY,
+    created_at      DATETIME(3)     NULL,
+    updated_at      DATETIME(3)     NULL,
+    deleted_at      DATETIME(3)     NULL,
 
-    room_id VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    owner_id BIGINT UNSIGNED,
-    is_active BOOLEAN DEFAULT TRUE,
-    password VARCHAR(255) DEFAULT NULL,
+    room_id         VARCHAR(255)    NOT NULL        UNIQUE,
+    name            VARCHAR(255),
+    owner_id        BIGINT          UNSIGNED,
+    is_active       BOOLEAN         DEFAULT TRUE,
+    password        VARCHAR(255)    DEFAULT NULL,
 
     INDEX idx_rooms_deleted_at (deleted_at)
 );
 
 CREATE TABLE audit_logs (
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    created_at DATETIME(3) NULL,
-    updated_at DATETIME(3) NULL,
-    deleted_at DATETIME(3) NULL,
+    id              BIGINT              UNSIGNED         AUTO_INCREMENT         PRIMARY KEY,
+    created_at      DATETIME(3)         NULL,
+    updated_at      DATETIME(3)         NULL,
+    deleted_at      DATETIME(3)         NULL,
 
-    actor_id VARCHAR(255),
-    actor_role VARCHAR(255),
-    action VARCHAR(255),
-    target_type VARCHAR(255),
-    target_id VARCHAR(255),
-    meta TEXT,
-    ip VARCHAR(255),
-    status VARCHAR(255),
+    actor_id        VARCHAR(255),
+    actor_role      VARCHAR(255),
+    action          VARCHAR(255),
+    target_type     VARCHAR(255),
+    target_id       VARCHAR(255),
+    meta            TEXT,
+    ip              VARCHAR(255),
+    status          VARCHAR(255),
 
     INDEX idx_audit_logs_deleted_at (deleted_at),
     INDEX idx_audit_logs_actor_id (actor_id),
@@ -49,23 +49,46 @@ CREATE TABLE audit_logs (
 
 CREATE TABLE notifications (
 
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id              BIGINT          UNSIGNED            AUTO_INCREMENT          PRIMARY KEY,
 
-    created_at DATETIME NULL,
-    updated_at DATETIME NULL,
-    deleted_at DATETIME NULL,
+    created_at      DATETIME        NULL,
+    updated_at      DATETIME        NULL,
+    deleted_at      DATETIME        NULL,
 
-    title VARCHAR(255) NOT NULL,
-    message TEXT NOT NULL,
+    title           VARCHAR(255)    NOT NULL,
+    message         TEXT            NOT NULL,
 
-    sender_id VARCHAR(255),
-    recipient_id VARCHAR(255),
+    sender_id       VARCHAR(255),
+    recipient_id    VARCHAR(255),
 
-    is_read BOOLEAN DEFAULT FALSE,
-    type VARCHAR(255) DEFAULT 'info',
+    is_read         BOOLEAN         DEFAULT FALSE,
+    type            VARCHAR(255)    DEFAULT 'info',
 
     INDEX idx_notifications_sender_id (sender_id),
     INDEX idx_notifications_recipient_id (recipient_id),
     INDEX idx_notifications_deleted_at (deleted_at)
+
+);
+
+CREATE TABLE messages (
+
+    id              BIGINT          UNSIGNED           AUTO_INCREMENT   PRIMARY KEY,
+
+    created_at      DATETIME(3)     NULL,
+    updated_at      DATETIME(3)     NULL,
+    deleted_at      DATETIME(3)     NULL,
+
+    sender_id       VARCHAR(255)    NOT NULL,
+    receiver_id     VARCHAR(255)    NOT NULL,
+
+    context         TEXT            NOT NULL,
+    message_type    VARCHAR(255)    DEFAULT 'text',
+
+    is_read         BOOLEAN         DEFAULT FALSE,
+    is_deleted      BOOLEAN         DEFAULT FALSE,
+
+    INDEX idx_messages_deleted_at (deleted_at),
+    INDEX idx_messages_sender_id (sender_id),
+    INDEX idx_messages_receiver_id (receiver_id)
 
 );
