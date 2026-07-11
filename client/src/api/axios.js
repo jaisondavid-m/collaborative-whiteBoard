8b9261331b980f8d4311d9e107523ec144bb0f70
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-07-11 20:33:23 +0530

    updated axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useAuthStore } from "../store/authStore.js"
 
 const API = axios.create({
-    baseURL: "https://collaborative-whiteboard-1-3p0d.onrender.com", //backend url
+    baseURL: "https://cw.bitsathy.in", //backend url
     headers: {
         "Content-Type":"application/json"
     }
